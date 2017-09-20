59ae3df9b5242b786a197e7fc83cd5d129dc24dd
xiaokun <838494268@qq.com>
2017-09-20 18:41:24 +0800

    给夜间模式增加一个渐变动画；

--- app/src/main/java/com/xiaocai/nightmode/ItemListActivity.java
+++ app/src/main/java/com/xiaocai/nightmode/ItemListActivity.java
@@ -1,8 +1,11 @@
 package com.xiaocai.nightmode;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -36,6 +39,7 @@ public class ItemListActivity extends AppCompatActivity
     public static final String NIGHT = "night";
     public static final String NIGHT_MODE = "data_day_night_mode";
     private Toolbar toolbar;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -67,6 +71,7 @@ public class ItemListActivity extends AppCompatActivity
         });
 
         recyclerView = (RecyclerView) findViewById(R.id.item_list);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
         assert recyclerView != null;
         setupRecyclerView(recyclerView);
 
@@ -104,24 +109,6 @@ public class ItemListActivity extends AppCompatActivity
     }
 
     /**
-     * 改变theme
-     */
-    private void changeThemeWithColorful()
-    {
-        if (!mPref.getBoolean(NIGHT, false))
-        {
-            colorful.setTheme(R.style.AppNightTheme);
-            StatusBarUtil.setColor(this, getResources().getColor(R.color.night_primary_color), 0);
-            mPref.edit().putBoolean(NIGHT, true).commit();
-        } else
-        {
-            colorful.setTheme(R.style.AppDayTheme);
-            StatusBarUtil.setColor(this, getResources().getColor(R.color.day_primary_color), 0);
-            mPref.edit().putBoolean(NIGHT, false).commit();
-        }
-    }
-
-    /**
      * 初始化
      */
     private void setupColorful()
@@ -136,6 +123,75 @@ public class ItemListActivity extends AppCompatActivity
 //                .backgroundColor(R.id.item_list, R.attr.root_view_bg)//前面设置了，这里就不必要设置了
                 .create();
     }
+
+    /**
+     * 改变theme
+     */
+    private void changeThemeWithColorful()
+    {
+        if (!mPref.getBoolean(NIGHT, false))
+        {
+            animChangeColor(R.style.AppNightTheme);
+            StatusBarUtil.setColor(this, getResources().getColor(R.color.night_primary_color), 0);
+            mPref.edit().putBoolean(NIGHT, true).commit();
+        } else
+        {
+            animChangeColor(R.style.AppDayTheme);
+            StatusBarUtil.setColor(this, getResources().getColor(R.color.day_primary_color), 0);
+            mPref.edit().putBoolean(NIGHT, false).commit();
+        }
+    }
+
+    /**
+     * 给夜间模式增加一个动画,颜色渐变
+     *
+     * @param newTheme
+     */
+    private void animChangeColor(final int newTheme)
+    {
+        final View rootView = getWindow().getDecorView();
+        rootView.setDrawingCacheEnabled(true);
+        rootView.buildDrawingCache(true);
+
+        final Bitmap localBitmap = Bitmap.createBitmap(rootView.getDrawingCache());
+        rootView.setDrawingCacheEnabled(false);
+        if (null != localBitmap && rootView instanceof ViewGroup)
+        {
+            final View tmpView = new View(this);
+            tmpView.setBackgroundDrawable(new BitmapDrawable(getResources(), localBitmap));
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            ((ViewGroup) rootView).addView(tmpView, params);
+            tmpView.animate().alpha(0).setDuration(400).setListener(new Animator.AnimatorListener()
+            {
+                @Override
+                public void onAnimationStart(Animator animation)
+                {
+                    colorful.setTheme(newTheme);
+                    System.gc();
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation)
+                {
+                    ((ViewGroup) rootView).removeView(tmpView);
+                    localBitmap.recycle();
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation)
+                {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation)
+                {
+
+                }
+            }).start();
+        }
+    }
+
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView)
     {
--- library/src/main/java/me/drakeet/support/about/ContributorViewBinder.java
+++ library/src/main/java/me/drakeet/support/about/ContributorViewBinder.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import me.drakeet.multitype.ItemViewBinder;
 
 /**
